2edf013732f02349c2c6991f3aaac8379ce6c458
tssabbir <tssabbirhosen@gmail.com>
2023-10-21 23:13:33 +0600

    Remove node_modules from version control

---
+++ FirstCode/index.js
@@ -0,0 +1,8 @@
+var fullName = "Sabbir Hosen";
+console.log(fullName);
+var sum = 0;
+var addNumber = function (num1, num2) {
+    sum = num1 + num2;
+    console.log(sum);
+};
+addNumber(12, 34);
---
+++ FirstCode/index.ts
@@ -0,0 +1,12 @@
+let fullName = "Sabbir Hosen";
+
+console.log(fullName);
+
+var sum = 0;
+const addNumber = (num1:number, num2:number
+  ) => {
+  sum = num1 + num2;
+  console.log(sum);
+};
+
+addNumber(12,34)
