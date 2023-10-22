d3647ef7e6df1c334f320c6f3e99551e1dcda1ca
tssabbit <tssabbirhosen@gmail.com>
2023-10-22 16:16:50 +0600

    check

--- FirstCode/index.ts
+++ FirstCode/index.ts
@@ -3,10 +3,9 @@ let fullName = "Sabbir Hosen";
 console.log(fullName);
 
 var sum = 0;
-const addNumber = (num1:number, num2:number
-  ) => {
+const addNumber = (num1: number, num2: number) => {
   sum = num1 + num2;
   console.log(sum);
 };
 
-addNumber(12,34)
+addNumber(12, 34);
